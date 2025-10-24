a0bb65232dd810e89f337774ac53a5f9d59789fd
Pelatform Dev <pelatformdev@gmail.com>
2025-10-24 23:09:22 +0700

    chore: update ignore patterns in biome.json and refine package.json scripts; add 'react' as external dependency in tsup.config.ts

--- packages/tsup.config.ts
+++ packages/tsup.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig(() => {
   return {
+    external: ['react'],
     entry: ['src/index.ts', 'src/components.ts', 'src/helpers.ts'],
     format: ['esm', 'cjs'],
     splitting: true,
